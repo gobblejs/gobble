ff019ec5e922e603e998df00e1086041f5560dca
Rich Harris <richard.a.harris@gmail.com>
2014-08-15 20:38:43 -0400

    add spelunk to example

--- example/gobblefile.js
+++ example/gobblefile.js
@@ -8,5 +8,6 @@ module.exports = [
 	src.exclude( 'scss/**' ).map( replace, { 'TITLE' : 'woop woop!' }),
 	src.transform( 'sass', { src: 'scss/main.scss', dest: 'min.css' }),
 	gobble( 'coffee' ).map( 'coffee' ),
-	gobble( 'js' ).map( 'esnext' )
+	gobble( 'js' ).map( 'esnext' ),
+	gobble( 'data' ).transform( 'spelunk' )
 ];
