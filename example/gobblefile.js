3e445023472154b628b7f7908e3a8f707f609d58
Rich Harris <richard.a.harris@gmail.com>
2014-08-19 18:42:20 -0400

    playing with example folder

--- example/gobblefile.js
+++ example/gobblefile.js
@@ -9,5 +9,19 @@ module.exports = [
 	gobble( 'js' ).map( 'esnext' ),
 	gobble( 'data' ).transform( 'spelunk', { dest: 'data5.json' }),
 	gobble( 'es6' ).map( 'esperanto', { type: 'amd', defaultOnly: true }),
-	gobble( 'ractive_components' ).map( 'ractive' )
+	gobble( 'ractive_components' ).map( 'ractive' )//.transform( throwError, { duration: 2000 })
 ];
+
+
+function delay ( inputDir, outputDir, options, done ) {
+	require( 'gobble' ).file.copydir( inputDir, outputDir );
+
+
+	setTimeout( done, options.duration || 5000 );
+}
+
+function throwError ( inputDir, outputDir, options, done ) {
+	setTimeout( function () {
+		done( 'an error happened' );
+	}, 1000 );
+}
