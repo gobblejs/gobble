318d96fad57c7582ae348fc8e942f653f0f6ae38
Rich Harris <richard.a.harris@gmail.com>
2014-08-27 00:05:15 -0400

    rig up build task

--- example/gobblefile.js
+++ example/gobblefile.js
@@ -4,7 +4,6 @@ var src = gobble( 'src' );
 
 module.exports = gobble([
 	src.exclude( 'scss/**' ).map( 'replace', { 'TITLE' : 'woop woop!' }),
-	src.map( 'replace', { 'TITLE' : 'woop woop!' }),
 	src.transform( 'sass', { src: 'scss/main.scss', dest: 'min.css' }),
 	gobble( 'coffee' ).map( 'coffee' ),
 	gobble( 'js' ).map( 'es6-transpiler' ),
