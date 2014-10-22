f7450fca32e14201f3176bc39861c42a2c03a697
Rich Harris <richard.a.harris@gmail.com>
2014-10-22 14:31:31 -0400

    fixes #16 - nodes cleanup after themselves on each successful build

--- test/scenarios.js
+++ test/scenarios.js
@@ -11,14 +11,18 @@ module.exports = function () {
 
 	describe( 'gobble', function () {
 		beforeEach( function () {
-			return sander.rimraf( __dirname, 'tmp' );
+			return sander.rimraf( __dirname, 'tmp' ).then( function () {
+				return sander.copydir( __dirname, 'sample' ).to( __dirname, 'tmp' );
+			});
 		});
 
 		afterEach( function () {
 			try {
 				return task.close().then( cleanup );
 			} catch ( err ) {
-
+				if ( task ) {
+					throw err;
+				}
 			} finally {
 				task = null;
 				return cleanup();
@@ -36,32 +40,63 @@ module.exports = function () {
 		});
 
 		it( 'should correctly copy cached transformations of unchanged files with file transformers that change extensions (#14)', function ( done ) {
+			var source = gobble( r( 'tmp/foo' ) );
+
 			function toTxt ( input ) {
 				return input;
 			}
 
 			toTxt.defaults = { accept: '.md', ext: '.txt' };
 
-			sander.copydir( __dirname, 'sample/foo' ).to( __dirname, 'tmp/foo' ).then( function () {
-				var source = gobble( r( 'tmp/foo' ) );
+			task = source.transform( toTxt ).serve();
 
-				task = source.transform( toTxt ).serve();
+			task.once( 'ready', function () {
+				task.once( 'built', function () {
+					request( 'http://localhost:4567/foo.txt', function ( err, response, body ) {
+						assert.equal( body.trim(), 'foo: this is some text' );
+						done();
+					});
+				});
 
-				task.once( 'ready', function () {
-					task.once( 'built', function () {
-						request( 'http://localhost:4567/foo.txt', function ( err, response, body ) {
-							assert.equal( body.trim(), 'foo: this is some text' );
+				// simulate a file change
+				source.emit( 'error', {
+					name: 'GobbleError',
+					code: 'INVALIDATED',
+					message: 'build invalidated',
+					changes: [{ type: 'change', path: r( 'tmp/foo/foo.md' ) }]
+				});
+			});
+		});
+
+		it( 'should clean up after itself (#16)', function ( done ) {
+			var source = gobble( r( 'tmp/foo' ) );
+
+			function foo ( inputdir, outputdir, options ) {
+				return sander.copydir( inputdir ).to( outputdir );
+			}
+
+			task = source.transform( foo ).serve();
+
+			task.once( 'ready', function () {
+				task.once( 'built', function () {
+					sander.readdir( '.gobble' ).then( function ( files ) {
+						// find foo tmpdir
+						files = files.filter( function ( file ) { return /-foo$/.test( file ); });
+
+						sander.readdir( '.gobble', files[0] ).then( function ( files ) {
+							assert.deepEqual( files, [ '2' ] );
 							done();
 						});
 					});
+				});
 
-					// simulate a file change
-					source.emit( 'error', {
-						name: 'GobbleError',
-						code: 'INVALIDATED',
-						message: 'build invalidated',
-						changes: [{ type: 'change', path: r( 'tmp/foo/foo.md' ) }]
-					});
+
+				// simulate a file change
+				source.emit( 'error', {
+					name: 'GobbleError',
+					code: 'INVALIDATED',
+					message: 'build invalidated',
+					changes: [{ type: 'change', path: r( 'tmp/foo/foo.md' ) }]
 				});
 			});
 		});
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 console.log( 'Running gobble.js tests. Please ensure that Gobble is not already running, otherwise tests may fail' );
 
+process.chdir( __dirname );
+
 require( './env' )();
 require( './build' )();
 require( './serve' )();
