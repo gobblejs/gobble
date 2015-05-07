19fbbddadf98a71e8811baacb7fb30bf88d4a0be
Rich Harris <richard.a.harris@gmail.com>
2015-05-06 20:34:32 -0400

    add another error location extraction heuristic

--- src/utils/extractLocationInfo.js
+++ src/utils/extractLocationInfo.js
@@ -1,4 +1,16 @@
-export default function extractLocationInfo ({ file, line, column, message, loc }) {
+/**
+ * Extracts location info from error objects using a set of heuristics
+   based on real-world examples
+ * @param {object} err - an error object thrown by a transpiler
+ * @returns {file, line, column}
+ */
+export default function extractLocationInfo ( err ) {
+	let { file, line, column, message, loc } = err;
+
+	if ( !file && err.filename ) {
+		file = err.filename;
+	}
+
 	if ( line === undefined && column === undefined && loc ) {
 		line = loc.line;
 		column = loc.column;
@@ -15,6 +27,17 @@ export default function extractLocationInfo ({ file, line, column, message, loc 
 	if ( column === undefined ) {
 		if ( match = /column (\d+)/.exec( message ) ) {
 			column = +match[1];
+		}
+	}
+
+	// Handle errors from e.g. browserify
+	// Unexpected token (123:456) while parsing /path/to/.gobble/12-derequire/1/app.js
+	if ( line === undefined && column === undefined ) {
+		const match = /(\d+):(\d+)/.exec( message );
+
+		if ( match ) {
+			line = match[1];
+			column = match[2];
 		}
 	}
 
