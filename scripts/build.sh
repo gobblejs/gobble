cc7422daf11d42eecf0b36709f605b69b596c263
flames of love <mechanicofthesequence@gmail.com>
2015-10-04 03:30:10 +0200

    bump deps & use babel
     - fix mime (lookup is now a obj method)
     - drop esperanto in favour of babel
     - fix babel's this before super complaints

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,14 +3,4 @@ set -e
 rm -rf lib
 mkdir -p lib
 
-echo "babel..."
-babel src --out-dir .babel -b es6.modules,useStrict --source-maps-inline --loose es6.classes > /dev/null
-
-echo "esperanto..."
-esperanto -i .babel -o .esperanto -m inline -t cjs -s
-
-echo "sorcery"
-sorcery -i .esperanto -o lib
-
-rm -rf .babel
-rm -rf .esperanto
+babel src -m commonStrict --out-dir lib -b useStrict --source-maps-inline --loose es6.classes
