d56db5b8b21e6c9174c41344688237f10583b885
Rich-Harris <richard.a.harris@gmail.com>
2015-10-18 19:04:08 -0400

    bump rollup-babel version, reinstate sourcemaps

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,4 +3,4 @@ set -e
 rm -rf dist
 mkdir -p dist
 
-rollupbabel -i src/index.js -o dist/gobble.js -f cjs -e path,chalk,sander,chokidar,debounce,promise-map-series,eventemitter2,buffer-crc32,require-relative,util,http,tiny-lr,minimatch,url,mime,graceful-fs
+rollupbabel -i src/index.js -o dist/gobble.js -f cjs -m -e path,chalk,sander,chokidar,debounce,promise-map-series,eventemitter2,buffer-crc32,require-relative,util,http,tiny-lr,minimatch,url,mime,graceful-fs
