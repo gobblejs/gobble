18b7ffa79e30fbcd75113de1e09885f35700653c
Rich-Harris <richard.a.harris@gmail.com>
2016-04-22 21:47:36 -0400

    simplify build process

--- scripts/build.sh
+++
@@ -1,6 +0,0 @@
-set -e
-
-rm -rf dist
-mkdir -p dist
-
-rollupbabel -i src/index.js -o dist/gobble.js -f cjs -m -e path,chalk,sander,pathwatcher,debounce,promise-map-series,eventemitter2,buffer-crc32,require-relative,util,http,tiny-lr,minimatch,url,mime,graceful-fs
